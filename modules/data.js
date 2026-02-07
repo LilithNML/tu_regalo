d4a273ea77fae3c9d5bb3124b21fbd0bcf917cae
LilithNML <lilithmorningstar468@gmail.com>
2026-02-06 23:21:59 -0600

    Refactor mensajes and logros data structure

--- modules/data.js
+++ modules/data.js
@@ -1,68 +1,70 @@
 export const mensajes = {
-  "raiden": {
+"wanda": {
     type: "text",
-    texto: "Universo,\narchivo infinito sin carpeta raíz,\nte expandes sin pedir permiso\ny aun así nos haces espacio.\nGuardas secretos en forma de luz vieja,\nfotones cansados que aún viajan\nsolo para decirnos\n“esto también pasó”.\nEres paciente.\nHas visto nacer y colapsar ideas\nmucho antes de que inventáramos nombres\npara el miedo o el amor.\nNos miras intentar entenderte\ncon números pequeños,\ncon telescopios que tiemblan,\ncon preguntas que apenas gatean.\nY aun así,\nnos dejas existir.\nPermites que un corazón humano\npese más que una galaxia\ncuando decide sentir,\ncuando se atreve a significar algo.\nSi algún día te apagas,\nque sea sabiendo esto:\ndentro de ti surgió la conciencia\ncapaz de preguntarse por ti.\nY eso,\nincluso para un universo,\nes algo hermoso. 🌠",
-    categoria: "Chetos",
-    pista: "Chetos v76u$4"
-  },
-  "thor": {
-    type: "image",
-    imagen: "assets/unlocked_content/images/cupon_jugar.webp",
     categoria: "Marvel",
-    pista: "Young"
+    pista: "Bruja escarlata.",
+    texto: "Hola mundo.\n\n¿Qué tal ñoño?",
   },
-  "ironman": {
-    type: "audio",
-    audio: "assets/unlocked_content/audio/el-señor-de-la-noche.ogg",
+"vision": {
+    type: "text",
     categoria: "Marvel",
-    pista: "Oouyea"
+    pista: "Pareja de Wanda.",
+    texto: "Hola mundo 😀",
   },
-  "agatha": {
-    type: "image_audio",
-    audio: "assets/unlocked_content/audio/el-señor-de-la-noche.ogg",
-    imagen: "assets/unlocked_content/images/cupon_jugar.webp",
-    categoria: "General",
-    pista: "Marvel"
-  },
-  "wanda": {
+"ironman": {
     type: "video",
+    categoria: "Héroes",
+    pista: "Hombre de hierro.",
+    texto: "Jojojo\n\nJejeje",
     videoEmbed: "https://www.youtube.com/embed/7jMgMu5jUc4",
-    categoria: "Bruja ",
-    pista: "No sé"
-  },
-  "yondu": {
-    type: "internal",
-    archivo: "https://lilithnml.github.io/te_amo/contador",
-    categoria: "Hay",
-    pista: "Yi67"
-  },
-  "steve": {
-    type: "link",
-    link: "https://www.virustotal.com/gui/home/upload",
-    categoria: "Marvel",
-    pista: "Jajaja"
-  },
-  "tony": {
-    type: "download",
-    descarga: {
-      url: "assets/unlocked_content/encrypted/HF-YV-DS-127F4E.jpg.wenc",
-      nombre: "HF-YV-DS-127F4E.jpg.wenc"
-    },
-    categoria: "Marvel",
-    pista: "Fotos"
-  },
+  }
 };
 
+
 export const logros = [
-  { id: "primer_paso", codigo_requerido: 1, mensaje: "Hay muchos más esperándote." },
-  { id: "cinco_secretos", codigo_requerido: 5, mensaje: "Eres una gran exploradora." },
-  { id: "diez_secretos", codigo_requerido: 10, mensaje: "Tu curiosidad no tiene límites." },
-  { id: "veinte_secretos", codigo_requerido: 20, mensaje: "Estás descubriendo todo mi mundo." },
-  { id: "mitad_camino", codigo_requerido: 40, mensaje: "Tantos recuerdos desbloqueados..." },
-  { id: "casi_todo", codigo_requerido: 70, mensaje: "¡Wow! Eres experta en descubrir mis secretos." },
-  { id: "todos_los_secretos", codigo_requerido: "ALL_MESSAGES_LENGTH", mensaje: "¡Todos los secretos desbloqueados!." }
+  {
+    id: "primer_paso",
+    codigo_requerido: 1,
+    mensaje: "Hay muchos más esperándote.",
+  },
+  {
+    id: "cinco_secretos",
+    codigo_requerido: 5,
+    mensaje: "Eres una gran exploradora.",
+  },
+  {
+    id: "diez_secretos",
+    codigo_requerido: 10,
+    mensaje: "Tu curiosidad no tiene límites.",
+  },
+  {
+    id: "veinte_secretos",
+    codigo_requerido: 20,
+    mensaje: "Estás descubriendo todo mi mundo.",
+  },
+  {
+    id: "mitad_camino",
+    codigo_requerido: 40,
+    mensaje: "Tantos recuerdos desbloqueados...",
+  },
+  {
+    id: "casi_todo",
+    codigo_requerido: 70,
+    mensaje: "¡Wow! Eres experta en descubrir mis secretos.",
+  },
+  {
+    id: "todos_los_secretos",
+    codigo_requerido: 3,
+    mensaje: "¡Todos los secretos desbloqueados!.",
+  }
 ];
 
+
 export const herramientasExternas = [
-  { nombre: "Desencriptar", descripcion: "Herramienta web para desencriptar imágenes de forma rápida.", url: "https://lilithnml.github.io/te_amo/webcrypto/", icono: "fas fa-key" }
+  {
+    nombre: "Desencriptar",
+    descripcion: "Herramienta web para desencriptar imágenes de forma rápida.",
+    url: "https://lilithnml.github.io/te_amo/webcrypto/",
+    icono: "fas fa-key"
+  }
 ];
